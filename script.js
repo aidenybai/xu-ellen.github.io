f1111661f5b4f255eef17bee2a020c1306d55fb0
Aiden Bai <aiden.bai05@gmail.com>
2022-05-08 17:27:07 -0700

    Fix scripts not running upon navigation

--- script.js
+++ script.js
@@ -2,23 +2,28 @@ import { router } from 'https://unpkg.com/million@1.8.9-0/dist/router.mjs';
 
 router('body');
 
-const menuIcon = document.querySelector(".hamburger-menu");
-const navbar = document.querySelector(".navbar");
+// Handles each navigation
+window.addEventListener('million:navigate', () => {
+  requestAnimationFrame(() => {
+    const menuIcon = document.querySelector('.hamburger-menu');
+    const navbar = document.querySelector('.navbar');
 
-menuIcon.addEventListener("click", () => {
-	navbar.classList.toggle("change");
-});
+    menuIcon.addEventListener('click', () => {
+      navbar.classList.toggle('change');
+    });
 
-const pics = document.querySelectorAll(".pic");
-const col = document.querySelectorAll(".col");
+    const pics = document.querySelectorAll('.pic');
+    const col = document.querySelectorAll('.col');
 
-function click(i) {
-	col[i].classList.toggle("change");
-	pics[i].classList.toggle("no-hover");
-}
+    function click(i) {
+      col[i].classList.toggle('change');
+      pics[i].classList.toggle('no-hover');
+    }
 
-Array.prototype.forEach.call(pics, function (pic, index) {
-	pic.addEventListener("click", function () {
-		click(index + 1);
-	});
+    Array.prototype.forEach.call(pics, function (pic, index) {
+      pic.addEventListener('click', function () {
+        click(index + 1);
+      });
+    });
+  });
 });
